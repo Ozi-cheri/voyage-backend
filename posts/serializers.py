e89da26ee9a908907867bd91dc48a742a6255dfc
Sylveria Ozioma Ihemedu <silvernasa@myyahoo.comclear>
2025-03-23 22:22:25 +0100

    Add followers, likes to profilesand posts, modify posts urls.py

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,27 +1,34 @@
 from rest_framework import serializers
-from .models import Post
+from posts.models import Post
+from likes.models import Like
+
 
 class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
-
-
-
+    like_id = serializers.SerializerMethodField()
 
 
     def get_is_owner(self, obj):
-        request = self.context.get('request')  
-        if request and request.user.is_authenticated:
-            return request.user == obj.owner  # Returns True if the user owns the profile
-        return False  # Returns False for all other profiles
+        request = self.context['request']
+        return request.user == obj.owner
+
+    def get_like_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            like = Like.objects.filter(
+                owner=user, post=obj
+            ).first()
+            return like.id if like else None
+        return None
 
     class Meta:
         model = Post
         fields = [
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
-            'title', 'content', 'image', 
+            'title', 'content', 'image', 'like_id',
+            
         ]
-
