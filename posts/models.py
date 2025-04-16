bf4121030a89819b894cc6e55cffd9b973c4cbc3
Sylveria Ozioma Ihemedu <silvernasa@myyahoo.comclear>
2025-04-16 20:58:56 +0200

    Modify profiles models amd posts models, update settings for redeployment

--- posts/models.py
+++ posts/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
     image = models.ImageField(
-        upload_to='images/', default='../default_post_minglv', blank=True
+        upload_to='images/', default='../default_post_mlqsjk', blank=True
     )
 
     created_at = models.DateTimeField(auto_now_add=True)
--- profiles/models.py
+++ profiles/models.py
@@ -6,12 +6,11 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-
     updated_at = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=255, blank=True)
     content = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to='images/', default='../default_profile_ve8mol'
+        upload_to='images/', default='../default_profile_wsvu9h'
     )
 
     class Meta:
@@ -24,6 +23,5 @@ class Profile(models.Model):
 def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(owner=instance)
-
 
 post_save.connect(create_profile, sender=User)
