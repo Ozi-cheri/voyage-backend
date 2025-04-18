35cd2d97d8f784f50d6d776c98d5eecc68c4a4b1
Sylveria Ozioma Ihemedu <silvernasa@myyahoo.comclear>
2025-04-18 20:11:10 +0200

    Modify profiles and posts, update settings for redeployment

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,4 @@
-from django.db.models import Count
-from rest_framework import generics, filters
-from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import generics
 from profiles.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
@@ -8,35 +6,12 @@ from .serializers import ProfileSerializer
 
 class ProfileList(generics.ListAPIView):
     
-    queryset = Profile.objects.annotate(
-        posts_count=Count('owner__post', distinct=True),
-        followers_count=Count('owner__followed', distinct=True),
-        following_count=Count('owner__following', distinct=True)
-    ).order_by('-created_at')
+    queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
-    filter_backends = [
-        filters.OrderingFilter,
-        DjangoFilterBackend,
-    ]
-
-    filterset_fields = [
-        'owner__following__followed__profile',
-    ]
-    ordering_fields = [
-        'posts_count',
-        'followers_count',
-        'following_count',
-        'owner__following__created_at',
-        'owner__followed__created_at',
-    ]
 
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
-    
+   
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Profile.objects.annotate(
-        posts_count=Count('owner__post', distinct=True),
-        followers_count=Count('owner__followed', distinct=True),
-        following_count=Count('owner__following', distinct=True)
-    ).order_by('-created_at')
+    queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
